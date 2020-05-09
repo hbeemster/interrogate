a95917b9a32ff30aa31e3ea04f90549e68aa5342
herbert <herbertbeemster@gmail.com>
2020-05-09 08:54:34 +0200

    - try out docstring content check

---
+++ tests/functional/sample/docstring_content/foo.py
@@ -0,0 +1,13 @@
+"""Sample module-level docs"""
+
+
+class Foo(object):
+    """Foo class"""
+
+    def method_foo(self):
+        """this method does foo"""
+        pass
+
+    def function_foo(self):
+        """this method does foo"""
+        return None
